190d5b78ff7faedc8ecee6c9ca5dfdcf07325449
Bassem Hussein <75940885+bassemhussein-code@users.noreply.github.com>
2022-07-24 21:23:19 +0200

    filtering functions

--- DataStorage/refactoringFunctions/query.js
+++ DataStorage/refactoringFunctions/query.js
@@ -1,12 +1,10 @@
 let todos = []
 
- const filtering = function(){
-    todos  = renderingLocaStorage()
-    createElement()
-}
 filtering()
 
 document.querySelector('#add-todo').addEventListener('click', function(){
     renderInputPara()
     filtering()
 });
+
+
